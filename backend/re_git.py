149721d42081440107858acbc4db30366b13eb68
jesswang9 <jesswang9.cm@gmail.com>
2022-01-28 10:26:27 +0000

    // documentation

--- backend/re_git.py
+++ backend/re_git.py
@@ -1,20 +1,10 @@
-import numpy as np
+# Procedure that carries out an inputted sequence of moves, in the format of an array, on the cube in memory:
 
 
-# axis: "X" "Y" "Z"; angle: 1(90CW) 2(180) -1(90ACW)
-def getRotationMatrix(axis, angle):
-    RotationMatrix = []
-    theta = angle * np.pi / 2
-    if axis == "X":
-        RotationMatrix = [[1, 0, 0],
-                          [0, np.cos(theta), np.sin(theta)],
-                          [0, np.sin(theta), np.cos(theta)]]
-    if axis == "Y":
-        RotationMatrix = [[np.cos(theta), 0, np.sin(theta)],
-                          [0, 1, 0],
-                          [np.sin(theta), 0, np.cos(theta)]]
-    if axis == "Z":
-        RotationMatrix = [[np.cos(theta), np.sin(theta), 0],
-                          [np.sin(theta), np.cos(theta), 0],
-                          [0, 0, 1]]
-    return RotationMatrix
+def doSequenceOfMoves(cube, sequence):
+    for move in sequence:
+        print("before:", repr(move), cube.toDict()["squares"][0])
+        print('----------')
+        cube.doMove(move)
+        print("after:", repr(move), cube.toDict()["squares"][0])
+        print('----------')git
