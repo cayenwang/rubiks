a7d5c11516978151ca76a6deddfb4d09663ef09b
jesswang9 <jesswang9.cm@gmail.com>
2022-02-04 11:44:26 +0000

    unneeded anymore

--- backend/re_git.py
+++
@@ -1,6 +0,0 @@
-# Procedure that carries out an inputted sequence of moves, in the format of an array, on the cube in memory:
-
-
-def doSequenceOfMoves(cube, sequence):
-    for move in sequence:
-        cube.doMove(move)
